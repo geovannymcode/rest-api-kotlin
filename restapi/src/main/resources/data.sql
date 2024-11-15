bd23d8ff1f10bbf26c939fb7abc52740d3ac8601
Geovanny Mendoza <10421376+geovannymcode@users.noreply.github.com>
2024-11-14 23:11:03 -0500

    Update data.sql

--- restapi/src/main/resources/data.sql
+++ restapi/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO PERSONA(documento, nombre, apellido, fecha_Nac)
 VALUES (123456,'Geovanny','Mendoza',TO_DATE('20-09-1990','dd-MM-yyyy')),
-       (654321,'Yohana','Guerra',TO_DATE('13-02-1995','dd-MM-yyyy'));
+       (654321,'Claudia','Gonzalez',TO_DATE('13-02-1995','dd-MM-yyyy'));
 
 INSERT INTO ARTICULO(titulo, contenido/*, autor_id*/)
 VALUES ('Kotlin','All Relation with Kotlin'/*,123456*/);
